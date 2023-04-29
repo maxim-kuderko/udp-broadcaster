e5267957f5c6fa6906727f396dc3216915a6f8ce
Maxim Kuderko <7356439+maxim-kuderko@users.noreply.github.com>
2023-04-29 13:28:30 +0300

    initial

--- main.go
+++ main.go
@@ -29,12 +29,11 @@ func main() {
 				fmt.Printf("%d\n", c.Swap(0))
 			}
 		}()
-		globalBuff := make([]byte, 4096)
 		for {
+			globalBuff := make([]byte, 1024)
 			n, addr, _ := conn.ReadFrom(globalBuff[:])
 			srv.Serve(globalBuff[:n], addr)
 			c.Add(1)
-			globalBuff = globalBuff[:0]
 		}
 	}()
 	done := make(chan os.Signal, 1)
