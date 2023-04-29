904c061f990f1bb6c77b16c6b210abb3eb521916
Maxim Kuderko <7356439+maxim-kuderko@users.noreply.github.com>
2023-04-29 13:15:37 +0300

    initial

--- main.go
+++ main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	conn, err := net.ListenUDP("udp", &net.UDPAddr{
-		Port: 5000,
-	})
-	if err != nil {
-		panic(err)
-	}
-	srv := &Server{
-		conn: conn,
-		fn: func(data []byte, addr net.Addr) {
-			fmt.Println(string(data))
-		}}
-	globalBuff := make([]byte, 4096)
-	for {
-		n, addr, _ := conn.ReadFrom(globalBuff)
-		srv.Serve(globalBuff[:n], addr)
-	}
+	go func() {
+		conn, err := net.ListenUDP("udp", &net.UDPAddr{
+			Port: 5000,
+		})
+		if err != nil {
+			panic(err)
+		}
+		srv := &Server{
+			conn: conn,
+			fn: func(data []byte, addr net.Addr) {
+				fmt.Println(string(data))
+			}}
+		globalBuff := make([]byte, 4096)
+		for {
+			n, addr, _ := conn.ReadFrom(globalBuff)
+			srv.Serve(globalBuff[:n], addr)
+		}
+	}()
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Kill, os.Interrupt)
+	fmt.Println(<-done)
+
 }
